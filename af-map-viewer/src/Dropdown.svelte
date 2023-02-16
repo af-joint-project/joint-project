406e1492bd14f5d0b5fe75f15bcccdffd98410bb
KhaledALAsadi <khaled.asadi.1998@gmail.com>
2023-02-16 08:09:02 +0100

    Deleted the old project and added a new one that works with dropdown and json file

---
+++ af-map-viewer/src/Dropdown.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let options = [];
+    let selectedOption = null;
+
+    function handleChange(event) {
+      const selectedItem = event.target.value;
+      if (selectedItem) {
+        const selectedOption = options.find(option => option.name === selectedItem);
+        console.log(selectedOption); // log matching option to console
+        
+      }else{
+        selectedOption = null
+      }
+    }
+  </script>
+  
+  <label for="select">Välj ett rum:</label>
+  <select id="select" name="select" on:change={handleChange}>
+    <option value="">Välj ett rum</option>
+    {#each options as option}
+      <option value={option.name}>{option.name}</option>
+    {/each}
+
+  </select>
+
+
