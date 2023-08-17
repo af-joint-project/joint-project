4a10e96807a8d23282413d9a258cace24d20fae2
Christian Hall <christian.hall@arbetsformedlingen.se>
2023-08-17 13:21:01 +0200

    remove parameters

--- src/routes/api/hello/+server.ts
+++ src/routes/api/hello/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestEvent } from '../hello/$types';
 
 export function GET({ url }: RequestEvent) {
-	const helloWorld: string = url.searchParams.get('helloworld') ?? '';
-	return new Response (JSON.stringify({message : helloWorld}));
+	const message: string = url.searchParams.get('message') ?? '';
+	return new Response (JSON.stringify({message : 'helloworld'}));
 }
