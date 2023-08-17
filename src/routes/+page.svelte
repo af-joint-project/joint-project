417e0ca9226e22b84088498463cc6fe600488b36
Christian Hall <christian.hall@arbetsformedlingen.se>
2023-08-17 13:30:02 +0200

    remove parameters and correct import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Hello from './Hello.svelte';
+	import Hello from '$lib/components/Hello.svelte';
 </script>
 
 <main>
--- src/routes/api/hello/+server.ts
+++ src/routes/api/hello/+server.ts
@@ -1,6 +1,5 @@
 import type { RequestEvent } from '../hello/$types';
 
 export function GET({ url }: RequestEvent) {
-	const message: string = url.searchParams.get('message') ?? '';
 	return new Response (JSON.stringify({message : 'helloworld'}));
 }
